4f8a83936af9ccabee3df9446b749d054db00251
jacksimuse <cjh930306@naver.com>
2021-02-22 17:27:55 +0900

    210222 최종 커밋

---
+++ chap03/Chap04/Chap04App/Chap04App/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap04App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = 37656234;
+            if (values % 2 == 0)
+            {
+                Console.WriteLine("짝수입니다");
+            }
+            else
+            {
+                Console.WriteLine("홀수입니다");
+            }
+            if (values % 3 == 0)
+            {
+                Console.WriteLine("3의 배수입니다");
+            }
+            else
+            {
+                Console.WriteLine("3의 배수가 아닙니다");
+            }
+            if (values % 7 == 0)
+            {
+                Console.WriteLine("7의 배수입니다");
+            }
+            else
+            {
+                Console.WriteLine("7의 배수가 아닙니다");
+            }
+        }
+    }
+}
---
+++ chap03/Chap04/Chap04App/IncDecTestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IncDecTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("증감연산자 테스트");
+
+            int result = 26;
+            Console.WriteLine($"현재 숫자는 {result}"); // 26
+            result += 3;
+            Console.WriteLine($"현재 숫자는 {result}"); // 29
+            result -= 10;
+            Console.WriteLine($"현재 숫자는 {result}"); // 19
+
+            Console.WriteLine($"현재 숫자는 {result++}"); // 19 출력을 하고 나서 1을 더함
+            Console.WriteLine($"현재 숫자는 {++result}"); // 21 앞에 계산을 받고 1을 더해서  출력
+        }
+    }
+}
