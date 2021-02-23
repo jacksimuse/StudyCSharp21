c5d5811eefc2498a2cd2174ef5671eb94fe8f1e8
jacksimuse <cjh930306@naver.com>
2021-02-23 12:37:14 +0900

    0223 오전 커밋

---
+++ Chap05App/LoopTestApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("While문");
+            //int a = 10;
+            //while (a > 0)
+            //{
+            //    Console.WriteLine($"숫자는  {a}");
+            //    a -= 2;
+            //}
+            //Console.WriteLine("출력종료!");
+
+            //do
+            //{
+            //    Console.WriteLine($"숫자는 {a}");
+            //    a++;
+            //} while (a < 10);
+
+            //Console.WriteLine("For문");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine($"i 값은 {i}");
+            //}
+
+            //for (int i = 10; i >= 0; i--)
+            //{
+            //    Console.WriteLine($"i 값은 {i}");
+            //}
+
+            Console.WriteLine("구구단 시작!");
+            for (int i = 2; i < 10; i++)
+            {
+                Console.WriteLine($"{i}단 시작!");
+                for (int j = 1; j < 10; j++) 
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine($"{i}단 끝!");
+            }
+        }
+    }
+}
---
+++ Chap05App/SwitchTestApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    string day = "화요일";
+
+            //    if (day == "월요일")
+            //    {
+            //        Console.WriteLine("월요일입니다.");
+            //    }
+            //    else if (day == "화요일") 
+            //    {
+            //        Console.WriteLine("화요일입니다.");
+            //    }
+            //     else if (day == "수요일") 
+            //    {
+            //        Console.WriteLine("수요일입니다.");
+            //    }
+            //     else if (day == "목요일") 
+            //    {
+            //        Console.WriteLine("목요일입니다.");
+            //    }
+            //     else if (day == "금요일") 
+            //    {
+            //        Console.WriteLine("금요일입니다.");
+            //    }
+            //     else if (day == "토요일") 
+            //    {
+            //        Console.WriteLine("토요일입니다.");
+            //    }
+            //     else if (day == "일요일") 
+            //    {
+            //        Console.WriteLine("일요일입니다.");
+            //    }
+            //    else
+            //        Console.WriteLine("요일이 아닙니다.");
+            //}
+
+            while (true)
+            {
+                Console.WriteLine("요일을 입력하세요 : ");
+                string line = Console.ReadLine();
+
+                switch (line)
+                {
+                    case "월요일":
+                        Console.WriteLine("월요일입니다");
+                        break;
+                    case "화요일":
+                        Console.WriteLine("화요일입니다");
+                        break;
+                    case "수요일":
+                        Console.WriteLine("수요일입니다");
+                        break;
+                    case "목요일":
+                        Console.WriteLine("목요일입니다");
+                        break;
+                    case "금요일":
+                        Console.WriteLine("금요일입니다");
+                        break;
+                    case "토요일":
+                        Console.WriteLine("토요일입니다");
+                        break;
+                    case "일요일":
+                        Console.WriteLine("일요일입니다");
+                        break;
+                    default:
+                        Console.WriteLine("요일이 아닙니다.");
+                        break;
+
+                }
+            }
+        }
+    }
+}
