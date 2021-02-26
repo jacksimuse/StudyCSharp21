51ee76253a517f9c0a87ad79a8aee1a1fc89746f
jacksimuse <cjh930306@naver.com>
2021-02-26 17:25:57 +0900

    2월 마지막 커밋

---
+++ Chap10/Chap10App/2DegreeArrayApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DegreeArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            //arr[0, 0] = 1;
+            //arr[0, 1] = 2;
+            //arr[0, 2] = 3;
+            //arr[1, 0] = 4;
+            //arr[1, 1] = 5;
+            //arr[1, 2] = 6;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"[{i} ,{j} ] : {arr[i,j]}\t");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Chap10/Chap10App/Chap10App/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap10App
+{
+    class Test
+    { }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 80, 74, 81, 90, 34 };
+
+            Console.WriteLine($"Type of array : {array.GetType()}");
+            Console.WriteLine($"Base Type of array : {array.GetType().BaseType}");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+
+            Console.WriteLine("크기변경");
+            Array.Resize(ref array, 6);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+
+            Console.WriteLine("81 인덱스 찾기");
+            int idx = Array.IndexOf(array, 81);
+            for (int i = array.Length - 1; i >= idx; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[idx] = 50;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+        }
+    }
+}
+            /* var idx = 0;
+             foreach (var item in array)
+             {
+                 Console.WriteLine($"{idx++}번째 값 : {item}");
+             }*/
+
+//            Console.WriteLine("오름차순 정렬");
+//            Array.Sort(array);
+//            for (int i = 0; i < array.Length; i++)
+//            {
+//                Console.WriteLine($"{i}번째 값 : {array[i]}");
+//            }
+
+//            Console.WriteLine("내림차순 정렬");
+//            Array.Reverse(array);
+//            for (int i = 0; i < array.Length; i++)
+//            {
+//                Console.WriteLine($"{i}번째 값 : {array[i]}");
+//            }
+
+//           int idx = Array.IndexOf(array, 34);
+//            Console.WriteLine($"34의 인덱스 : {idx}");
+
+//            idx = Array.IndexOf(array, 100);
+//            Console.WriteLine($"34의 인덱스 : {idx}");
+
+//            Console.WriteLine("배열 삭제");
+//            Array.Clear(array, 3, 2);
+//            for (int i = 0; i < array.Length; i++)
+//            {
+//                Console.WriteLine($"{i}번째 값 : {array[i]}");
+//            }
+//        }
+//    }
+//}
