117fb570a06aeab0aa8b46ae55c24b1b78f649b9
jacksimuse <cjh930306@naver.com>
2021-03-08 15:31:06 +0900

    20장 커밋

---
+++ Chap20/Chap20App/FIleCopyApp/FrmMain.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace FIleCopyAPp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSource = new System.Windows.Forms.TextBox();
+            this.TxtTarget = new System.Windows.Forms.TextBox();
+            this.BtnSource = new System.Windows.Forms.Button();
+            this.BtnTarget = new System.Windows.Forms.Button();
+            this.BtnAsyncCopy = new System.Windows.Forms.Button();
+            this.BtnSyncCopy = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.PrbCopy = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Target :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TxtSource
+            // 
+            this.TxtSource.Location = new System.Drawing.Point(78, 12);
+            this.TxtSource.Name = "TxtSource";
+            this.TxtSource.Size = new System.Drawing.Size(337, 21);
+            this.TxtSource.TabIndex = 2;
+            this.TxtSource.TextChanged += new System.EventHandler(this.TxtSource_TextChanged);
+            // 
+            // TxtTarget
+            // 
+            this.TxtTarget.Location = new System.Drawing.Point(78, 48);
+            this.TxtTarget.Name = "TxtTarget";
+            this.TxtTarget.Size = new System.Drawing.Size(337, 21);
+            this.TxtTarget.TabIndex = 3;
+            // 
+            // BtnSource
+            // 
+            this.BtnSource.Location = new System.Drawing.Point(421, 8);
+            this.BtnSource.Name = "BtnSource";
+            this.BtnSource.Size = new System.Drawing.Size(26, 27);
+            this.BtnSource.TabIndex = 4;
+            this.BtnSource.Text = "...";
+            this.BtnSource.UseVisualStyleBackColor = true;
+            this.BtnSource.Click += new System.EventHandler(this.BtnSource_Click);
+            // 
+            // BtnTarget
+            // 
+            this.BtnTarget.Location = new System.Drawing.Point(421, 47);
+            this.BtnTarget.Name = "BtnTarget";
+            this.BtnTarget.Size = new System.Drawing.Size(26, 21);
+            this.BtnTarget.TabIndex = 5;
+            this.BtnTarget.Text = "...";
+            this.BtnTarget.UseVisualStyleBackColor = true;
+            this.BtnTarget.Click += new System.EventHandler(this.BtnTarget_Click);
+            // 
+            // BtnAsyncCopy
+            // 
+            this.BtnAsyncCopy.Location = new System.Drawing.Point(21, 96);
+            this.BtnAsyncCopy.Name = "BtnAsyncCopy";
+            this.BtnAsyncCopy.Size = new System.Drawing.Size(119, 37);
+            this.BtnAsyncCopy.TabIndex = 6;
+            this.BtnAsyncCopy.Text = "Async Copy";
+            this.BtnAsyncCopy.UseVisualStyleBackColor = true;
+            this.BtnAsyncCopy.Click += new System.EventHandler(this.BtnAsyncCopy_Click);
+            // 
+            // BtnSyncCopy
+            // 
+            this.BtnSyncCopy.Location = new System.Drawing.Point(174, 96);
+            this.BtnSyncCopy.Name = "BtnSyncCopy";
+            this.BtnSyncCopy.Size = new System.Drawing.Size(119, 37);
+            this.BtnSyncCopy.TabIndex = 7;
+            this.BtnSyncCopy.Text = "Sync Copy";
+            this.BtnSyncCopy.UseVisualStyleBackColor = true;
+            this.BtnSyncCopy.Click += new System.EventHandler(this.BtnSyncCopy_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(327, 96);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(119, 37);
+            this.BtnCancel.TabIndex = 8;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // PrbCopy
+            // 
+            this.PrbCopy.Location = new System.Drawing.Point(21, 152);
+            this.PrbCopy.Name = "PrbCopy";
+            this.PrbCopy.Size = new System.Drawing.Size(426, 27);
+            this.PrbCopy.TabIndex = 9;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 206);
+            this.Controls.Add(this.PrbCopy);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnSyncCopy);
+            this.Controls.Add(this.BtnAsyncCopy);
+            this.Controls.Add(this.BtnTarget);
+            this.Controls.Add(this.BtnSource);
+            this.Controls.Add(this.TxtTarget);
+            this.Controls.Add(this.TxtSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Async File Copy App";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtSource;
+        private System.Windows.Forms.TextBox TxtTarget;
+        private System.Windows.Forms.Button BtnSource;
+        private System.Windows.Forms.Button BtnTarget;
+        private System.Windows.Forms.Button BtnAsyncCopy;
+        private System.Windows.Forms.Button BtnSyncCopy;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.ProgressBar PrbCopy;
+    }
+}
+
---
+++ Chap20/Chap20App/FIleCopyApp/FrmMain.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIleCopyAPp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtSource_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                TxtSource.Text = dialog.FileName;
+            }
+        }
+
+        private void BtnTarget_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                TxtTarget.Text = dialog.FileName;
+            }
+        }
+
+        private async void BtnAsyncCopy_Click(object sender, EventArgs e)
+        {
+            long totalCopied = await CopyAsync(TxtSource.Text, TxtTarget.Text);                                                              
+            MessageBox.Show($"{totalCopied}로 복사했습니다!", "비동기복사완료");
+        }
+
+        private void BtnSyncCopy_Click(object sender, EventArgs e)
+        {
+            long totalCopied = CopySync(TxtSource.Text, TxtTarget.Text); // 동기 파일 복사
+            MessageBox.Show($"{totalCopied}로 복사했습니다!", "동기복사완료");
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("취소!");
+        }
+
+        private long CopySync(string sourcePath, string targetPath)
+        {
+            BtnAsyncCopy.Enabled = false; // 비동기버튼 비활성화(Enable vs Disalbe)
+            long totalCopied = 0; // 전부 복사했는지
+
+            using (FileStream sourceStream = new FileStream(sourcePath, FileMode.Open)) // 존자하는 파일
+            {
+                using (FileStream targetStream = new FileStream(targetPath, FileMode.Create)) // 새로 생성
+                {
+                    byte[] buffer = new byte[1024]; // 1024(1KB) * 1024 ==> 1MB
+                    int nRead = 0;
+                    while ((nRead = sourceStream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        targetStream.Write(buffer, 0, nRead); // 복사
+                        totalCopied += nRead;
+
+                        // 프로그레스바에 복사 상태 진행표시
+                        PrbCopy.Value = (int) (totalCopied / sourceStream.Length) *100; // long끼리 연산을 하면 안되서 int로 형변환
+                    }
+                }
+            }
+            // copy 끝나면
+            BtnAsyncCopy.Enabled = true;
+            return totalCopied;
+        }
+        /// <summary>
+        /// 비동기 복사
+        /// </summary>
+        /// <param name="sourcePath"></param>
+        /// <param name="targetPath"></param>
+        /// <returns></returns>
+        private async Task<long> CopyAsync(string sourcePath, string targetPath)
+        {
+            BtnSyncCopy.Enabled = false; // 비동기버튼 비활성화(Enable vs Disalbe)
+            long totalCopied = 0; // 전부 복사했는지
+
+            using (FileStream sourceStream = new FileStream(sourcePath, FileMode.Open)) // 존자하는 파일
+            {
+                using (FileStream targetStream = new FileStream(targetPath, FileMode.Create)) // 새로 생성
+                {
+                    byte[] buffer = new byte[1024 * 1024]; // 1024(1KB) * 1024 ==> 1MB
+                    int nRead = 0;
+                    while ((nRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                    {
+                        targetStream.WriteAsync(buffer, 0, nRead); // 복사
+                        totalCopied += nRead;
+
+                        // 프로그레스바에 복사 상태 진행표시
+                        PrbCopy.Value = (int) ((totalCopied / sourceStream.Length) * 100); // long끼리 연산을 하면 안되서 int로 형변환
+                    }
+                }
+            }
+            // copy 끝나면
+            BtnSyncCopy.Enabled = true;
+            return totalCopied;
+        }
+    }
+}
